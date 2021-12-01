cd008cd7e321e30ee64f3cfcdd6cb771199198df
VenkteshV <venkkratos@gmail.com>
2021-12-01 16:56:53 +0530

    Update Search.js

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -33,7 +33,7 @@ export default function Search() {
       content: q
     };
 
-    axios.post( 'http://0.0.0.0:8000/paraphrase', {content:q}, {headers: { "Content-Type": "application/json" }})
+    axios.post( 'http://3.7.119.1:8000/paraphrase', {content:q}, {headers: { "Content-Type": "application/json" }})
         .then( response => {
             setResults(response);
             setResultCount(response.data.length);
